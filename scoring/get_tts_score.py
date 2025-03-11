8423a2578eaab4ca1f9e0d5d6c5bd3725b930f4a
pravin-X109 <pravin@tryimpel.com>
2025-03-11 14:58:48 +0000

    feat: Update TTS scoring logic

--- scoring/get_tts_score.py
+++ scoring/get_tts_score.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)  # Create a logger for this module
 
 def load_dataset():
 
-    NUMBER_OF_SAMPLES = 5
+    NUMBER_OF_SAMPLES = 40
 
     print("Sampling dataset")
     try:
